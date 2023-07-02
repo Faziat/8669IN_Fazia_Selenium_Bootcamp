a4f236694f6098a6ec25e4cbcb55cd2bbd6dca1f
Faziat <toubouachefazia@gmail.com>
2023-07-02 18:11:47 -0400

    test login passed

---
+++ com.bmwusa/src/main/java/authentication/DashboardPage.java
@@ -0,0 +1,14 @@
+package authentication;
+import base.BasePage;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class DashboardPage extends BasePage {
+    public DashboardPage(){
+        PageFactory.initElements(driver,this);
+    }
+    @FindBy(xpath = "//a[@id='accountsBtn']")
+    public WebElement myAccountButton;
+}
+
---
+++ com.bmwusa/src/test/java/testauthentication/TestAuthentication.java
@@ -0,0 +1,33 @@
+package testauthentication;
+
+import authentication.DashboardPage;
+import authentication.LoginPage;
+import base.BasePage;
+import homepage.HomePage;
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import utils.ExcelData;
+
+public class TestAuthentication extends BasePage {
+
+    @Test(priority= 1, groups= {"BAT"},dataProvider = "loginDataProvider")
+
+    public void testLogin(String email,String password){
+        HomePage homePage = new HomePage();
+        LoginPage loginPage=homePage.clickOnLoginLink();
+        DashboardPage dashboardPage=loginPage.doSign(email, password);
+        Assert.assertTrue(isElementVisible(dashboardPage.myAccountButton));
+
+    }
+    @DataProvider(name="loginDataProvider")
+    public String[][] loginDataProvider()
+    {
+
+        String path= System.getProperty("user.dir")+"\\src\\test\\resources\\test_data.xlsx";
+        ExcelData ex=new ExcelData(path);
+        String data[][]=ex.readStringArrays("doSignIn");
+        return data;
+
+    }
+}
