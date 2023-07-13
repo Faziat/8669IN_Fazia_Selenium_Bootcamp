e51312a3af7b66fcbaf357a9d7d1ca9a4a496d3c
Faziat <toubouachefazia@gmail.com>
2023-07-12 20:51:27 -0400

    initial commit

---
+++ com.expedia/src/main/java/homepage/HomePage.java
@@ -0,0 +1,8 @@
+package homepage;
+
+import base.BasePage;
+import org.openqa.selenium.support.PageFactory;
+
+public class HomePage extends BasePage {
+
+}
---
+++ com.expedia/src/test/java/test_homepage/TestHomePage.java
@@ -0,0 +1,7 @@
+package test_homepage;
+
+import base.BasePage;
+
+public class TestHomePage extends BasePage {
+
+}
