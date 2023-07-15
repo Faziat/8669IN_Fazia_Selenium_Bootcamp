a0fb418b53744cf61a1e387629765434d733e60d
Faziat <toubouachefazia@gmail.com>
2023-07-14 22:03:19 -0400

    initial commit

--- com.expedia/src/test/java/test_homepage/TestHomePage.java
+++ com.expedia/src/test/java/test_homepage/TestHomePage.java
@@ -1,7 +1,22 @@
 package test_homepage;
 
 import base.BasePage;
+import homepage.HomePage;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
 public class TestHomePage extends BasePage {
+    public TestHomePage() {
+        PageFactory.initElements(driver, this);
+    }
+
+   @Test(priority = 1)
+    public void testNavigationToApplication() {
+      HomePage homepage = new HomePage();
+
+     Assert.assertTrue(isElementVisible(homepage.logo));
+
+    }
 
 }
